d0ab960b42ae6e4db40f8b5184055e1383e83a98
Natthawat Narin <natthawat.narin@gmail.com>
2025-01-16 10:37:56 +0700

    *

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Enable SWC minification for better performance
-  swcMinify: true,
-  // Disable x-powered-by header for security
-  poweredByHeader: false,
 }
 
 module.exports = nextConfig
